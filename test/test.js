c535391e10fca7b96d9bec1ba962697d7c1f6be6
Stalin Bonilla <starlin66@gmail.com>
2018-08-21 13:06:03 -0400

    feat: add allowEmpty option to not emit data when nothing has passed

--- test/test.js
+++ test/test.js
@@ -1,227 +1,266 @@
-
-var PassThrough = require('readable-stream/passthrough')
-var assert = require('assert')
-var cat = require('cat-stream')
+var PassThrough = require('readable-stream/passthrough');
+var assert = require('assert');
+var cat = require('cat-stream');
 var sinon = require('sinon');
 
-var Stringify = require('..')
+var Stringify = require('..');
 
-describe('Streamify()', function () {
-  it('should work with an empty stream', function (done) {
+describe('Streamify()', function() {
+  it('should work with an empty stream', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    stream.pipe(Stringify()).pipe(cat(function (err, buf) {
-      assert.ifError(err)
-      assert.equal(buf.toString('utf8'), '[\n\n]\n')
-      done()
-    }))
+    stream.pipe(Stringify()).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(buf.toString('utf8'), '[\n\n]\n');
+        done();
+      })
+    );
 
-    stream.end()
-  })
+    stream.end();
+  });
 
-  it('should work with a stream of length 1', function (done) {
+  it('should not send data when allowEmpty is set to true', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    stream.pipe(Stringify()).pipe(cat(function (err, buf) {
-      assert.ifError(err)
-      assert.equal(buf.toString('utf8'), '[\n{}\n]\n')
-      done()
-    }))
+    stream.pipe(Stringify({ allowEmpty: true })).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(buf.toString('utf8'), '');
+        done();
+      })
+    );
 
-    stream.write({})
-    stream.end()
-  })
+    stream.end();
+  });
 
-  it('should work with a stream of length 2', function (done) {
+  it('should work with a stream of length 1', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    stream.pipe(Stringify()).pipe(cat(function (err, buf) {
-      assert.ifError(err)
-      assert.equal(buf.toString('utf8'), '[\n{}\n,\n{}\n]\n')
-      done()
-    }))
+    stream.pipe(Stringify()).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(buf.toString('utf8'), '[\n{}\n]\n');
+        done();
+      })
+    );
 
-    stream.write({})
-    stream.write({})
-    stream.end()
-  })
+    stream.write({});
+    stream.end();
+  });
 
-  it('should work with non-objects', function (done) {
+  it('should work with a stream of length 2', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    stream.pipe(Stringify()).pipe(cat(function (err, buf) {
-      assert.ifError(err)
-      assert.equal(buf.toString('utf8'), '[\n"hello"\n]\n')
-      done()
-    }))
+    stream.pipe(Stringify()).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(buf.toString('utf8'), '[\n{}\n,\n{}\n]\n');
+        done();
+      })
+    );
 
-    stream.write('hello')
-    stream.end()
-  })
+    stream.write({});
+    stream.write({});
+    stream.end();
+  });
 
-  it('should return a string if encoding: "utf8"', function (done) {
+  it('should work with non-objects', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    stream.pipe(Stringify({
-      encoding: 'utf8'
-    })).once('data', function (chunk) {
-      assert.equal(typeof chunk, 'string')
-      done()
-    })
+    stream.pipe(Stringify()).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(buf.toString('utf8'), '[\n"hello"\n]\n');
+        done();
+      })
+    );
 
-    stream.write({})
-    stream.end()
-  })
+    stream.write('hello');
+    stream.end();
+  });
 
-  it('should allow a space argument to JSON.stringify()', function (done) {
+  it('should return a string if encoding: "utf8"', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    var stringify = Stringify()
-    stringify.space = 2
+    stream
+      .pipe(
+        Stringify({
+          encoding: 'utf8'
+        })
+      )
+      .once('data', function(chunk) {
+        assert.equal(typeof chunk, 'string');
+        done();
+      });
+
+    stream.write({});
+    stream.end();
+  });
+
+  it('should allow a space argument to JSON.stringify()', function(done) {
+    var stream = new PassThrough({
+      objectMode: true
+    });
+
+    var stringify = Stringify();
+    stringify.space = 2;
 
     var obj = {
       a: 1
-    }
+    };
 
-    stream
-    .pipe(stringify)
-    .pipe(cat(function (err, buf) {
-      assert.ifError(err)
-      assert.equal(buf.toString('utf8'), '[\n' + JSON.stringify(obj, null, 2) + '\n]\n')
+    stream.pipe(stringify).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(
+          buf.toString('utf8'),
+          '[\n' + JSON.stringify(obj, null, 2) + '\n]\n'
+        );
 
-      done()
-    }))
+        done();
+      })
+    );
 
-    stream.end(obj)
-  })
-  it('should allow a space argument to JSON.stringify()', function (done) {
+    stream.end(obj);
+  });
+  it('should allow a space argument to JSON.stringify()', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    var stringify = Stringify({space:2})
+    var stringify = Stringify({ space: 2 });
 
     var obj = {
       a: 1
-    }
+    };
 
-    stream
-      .pipe(stringify)
-      .pipe(cat(function (err, buf) {
-        assert.ifError(err)
-        assert.equal(buf.toString('utf8'), '[\n' + JSON.stringify(obj, null, 2) + '\n]\n')
+    stream.pipe(stringify).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(
+          buf.toString('utf8'),
+          '[\n' + JSON.stringify(obj, null, 2) + '\n]\n'
+        );
 
-        done()
-      }))
+        done();
+      })
+    );
 
-    stream.end(obj)
-  })
+    stream.end(obj);
+  });
 
-  it('should allow a space argument to JSON.stringify()', function (done) {
+  it('should allow a space argument to JSON.stringify()', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    var replacer = function(key, value){
-      if(key === 'a') return undefined
-      return value
-    }
+    var replacer = function(key, value) {
+      if (key === 'a') return undefined;
+      return value;
+    };
 
-    var stringify = Stringify({replacer:replacer})
+    var stringify = Stringify({ replacer: replacer });
 
     var obj = {
       a: 1
-    }
+    };
 
-    stream
-      .pipe(stringify)
-      .pipe(cat(function (err, buf) {
-        assert.ifError(err)
-        assert.equal(buf.toString('utf8'), '[\n' + JSON.stringify({}, null, 2) + '\n]\n')
+    stream.pipe(stringify).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(
+          buf.toString('utf8'),
+          '[\n' + JSON.stringify({}, null, 2) + '\n]\n'
+        );
 
-        done()
-      }))
+        done();
+      })
+    );
 
-    stream.end(obj)
-  })
+    stream.end(obj);
+  });
 
-  it('should allow custom openings and closings', function (done) {
+  it('should allow custom openings and closings', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    var stringify = Stringify({opener: "{\"test\": [\n", closer: "\n]}\n"})
+    var stringify = Stringify({ opener: '{"test": [\n', closer: '\n]}\n' });
 
-    var obj = [{a: 1}]
+    var obj = [{ a: 1 }];
 
-    stream
-      .pipe(stringify)
-      .pipe(cat(function (err, buf) {
-        assert.ifError(err)
-        assert.equal(buf.toString('utf8'), "{\"test\": [\n" + JSON.stringify(obj, null) + "\n]}\n")
-        
-        done()
-      }))
+    stream.pipe(stringify).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(
+          buf.toString('utf8'),
+          '{"test": [\n' + JSON.stringify(obj, null) + '\n]}\n'
+        );
 
-    stream.end(obj)
-  })
+        done();
+      })
+    );
 
-  it('should allow custom seperators', function (done) {
+    stream.end(obj);
+  });
+
+  it('should allow custom seperators', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
-    var stringify = Stringify({seperator: ' , '})
+    var stringify = Stringify({ seperator: ' , ' });
 
-    stream
-      .pipe(stringify)
-      .pipe(cat(function (err, buf) {
-        assert.ifError(err)
-        assert.equal(buf.toString('utf8'), "[\n1 , 2 , 3\n]\n")
-        
-        done()
-      }))
+    stream.pipe(stringify).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.equal(buf.toString('utf8'), '[\n1 , 2 , 3\n]\n');
 
-    stream.write(1)
-    stream.write(2)
-    stream.write(3)
-    stream.end()
-  })
+        done();
+      })
+    );
+
+    stream.write(1);
+    stream.write(2);
+    stream.write(3);
+    stream.end();
+  });
 
   it('should allow custom stringifiers', function(done) {
     var stream = new PassThrough({
       objectMode: true
-    })
+    });
 
     var stringifier = sinon.spy(JSON.stringify);
 
-    var stringify = Stringify({stringifier: stringifier})
+    var stringify = Stringify({ stringifier: stringifier });
 
-    stream
-      .pipe(stringify)
-      .pipe(cat(function (err, buf) {
-        assert.ifError(err)
-        assert.ok(stringifier.called)
-        assert.equal(buf.toString('utf8'), "[\n1\n,\n2\n,\n3\n]\n")
+    stream.pipe(stringify).pipe(
+      cat(function(err, buf) {
+        assert.ifError(err);
+        assert.ok(stringifier.called);
+        assert.equal(buf.toString('utf8'), '[\n1\n,\n2\n,\n3\n]\n');
 
-        done()
-      }))
+        done();
+      })
+    );
 
-    stream.write(1)
-    stream.write(2)
-    stream.write(3)
-    stream.end()
-  })
-})
+    stream.write(1);
+    stream.write(2);
+    stream.write(3);
+    stream.end();
+  });
+});
